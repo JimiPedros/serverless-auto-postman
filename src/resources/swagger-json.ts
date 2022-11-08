7edbe85d21585a180078a31fcbc8c62922dd72cb
James Peters <jamescpeters@outlook.com>
2022-11-09 09:07:35 +1300

    feat: remove swagger resources

--- src/resources/swagger-json.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const swagger = require('./swagger');
-
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(swagger),
-  };
-};
